a0025f6378d8102c5637c61087ce102452ad1e9b
Ben Siggery <ben.siggery@just-eat.com>
2018-07-18 18:56:40 +0100

    initial commit

---
+++ Assets/ButtonEventHandlerScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ButtonEventHandlerScript : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject obj = GameObject.FindGameObjectWithTag("clickableCube");
+            obj.GetComponent<Renderer>().material.color = Color.blue;
+        }
+    }
+}
